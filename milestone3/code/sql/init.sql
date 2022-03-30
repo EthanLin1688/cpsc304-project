c08a0da2da77849a02a4b70872198e17fc5647f4
Anton Chen <contact@antonchen.ca>
2022-03-29 17:51:41 -0700

    comment out

--- milestone3/code/sql/init.sql
+++ milestone3/code/sql/init.sql
@@ -218,91 +218,91 @@ ALTER TABLE OnlineAssessment ADD (
 --         ON DELETE CASCADE
 -- );
 
--- Populate tables
-INSERT ALL
-INTO Applicant VALUES (1, 'Steven', 'Li')
-INTO Applicant VALUES (2, 'Ethan', 'Lin') 
-INTO Applicant VALUES (3, 'Anton', 'Chen') 
-INTO Applicant VALUES (4, 'Raymond', 'Ng')
-INTO Applicant VALUES (5, 'Kanye', 'West')
-INTO Company VALUES ('Amazon', 'Vine', 'Vancouver', 'BC', 'Canada', 'V2A3A6')
-INTO Company VALUES ('Google', 'Heather', 'Vancouver', 'BC', 'Canada', 'V6H7A6')
-INTO Company VALUES ('Asana', 'Victoria', 'Vancouver', 'BC', 'Canada', 'V2H7U6')
-INTO Company VALUES ('Citadel', 'Queen', 'Vancouver', 'BC', 'Canada', 'V1N4B6')
-INTO Company VALUES ('Rippling', 'Oak', 'Vancouver', 'BC', 'Canada', 'V7A5A6')
-INTO Posting VALUES (1, 'Internship', 10, TO_DATE('10/22/2022', 'MM/DD/YYYY'), 'janitor', 'company bathroom', 'Asana')
-INTO Posting VALUES (2, 'Internship', 50, TO_DATE('5/1/2022', 'MM/DD/YYYY'), 'secretary', 'office', 'Google')
-INTO Posting VALUES (3, 'FullTime', 100, TO_DATE('12/4/2022', 'MM/DD/YYYY'), 'sales', 'office', 'Amazon')
-INTO Posting VALUES (4, 'FullTime', 500, TO_DATE('10/30/2022', 'MM/DD/YYYY'), 'worker', 'warehouse', 'Amazon')
-INTO Posting VALUES (5, 'Internship', 30, TO_DATE('2/28/2023', 'MM/DD/YYYY'), 'sorting documents', 'office', 'Google')
-INTO Recruiter VALUES (1, 'Kevin', 'Durant', 'Asana')
-INTO Recruiter VALUES (2, 'Kyrie', 'Irving', 'Google')
-INTO Recruiter VALUES (3, 'Jeff', 'Bezos', 'Amazon')
-INTO Recruiter VALUES (4, 'Lil', 'Pump', 'Rippling')
-INTO Recruiter VALUES (5, 'Kanye', 'West', 'Google')
-INTO Interviewer VALUES (1, 'James', 'Harden', 'Senior Engineer', 'Asana')
-INTO Interviewer VALUES (2, 'LeBron', 'James', 'Junior Engineer', 'Google')
-INTO Interviewer VALUES (3, 'Michael', 'Jordan', 'Junior Engineer', 'Amazon')
-INTO Interviewer VALUES (4, 'Stephen', 'Curry', 'Project Manager', 'Rippling')
-INTO Interviewer VALUES (5, 'Chris', 'Paus', 'Principal Engineer', 'Google')
-INTO JobApplication VALUES (1, 'stevenli.com/coverletter.pdf', 'stevenli.com/resume.pdf', 'Offer', 1, 1, 1, 'Asana')
-INTO JobApplication VALUES (2, 'ethanlin.com/coverletter.pdf', 'ethanlin.com/resume.pdf', 'Accepted', 2, 2, 2, 'Google')
-INTO JobApplication VALUES (3, 'antonchen.com/coverletter.pdf', 'antonchen.com/resume.pdf', 'Rejected', 3, 3, 3, 'Amazon')
-INTO JobApplication VALUES (4, 'antonchen.com/coverletter.pdf', 'antonchen.com/resume.pdf', 'Rejected', 3, 2, 2, 'Google')
-INTO JobApplication VALUES (5, 'antonchen.com/coverletter.pdf', 'antonchen.com/resume.pdf', 'Rejected', 3, 1, 1, 'Asana')
-INTO InformationSession VALUES (1, 'zoom', TO_DATE('5/1/2022', 'MM/DD/YYYY'), 'Asana')
-INTO InformationSession VALUES (2, 'zoom', TO_DATE('5/10/2022', 'MM/DD/YYYY'), 'Google')
-INTO InformationSession VALUES (3, 'zoom', TO_DATE('10/10/2022', 'MM/DD/YYYY'), 'Amazon')
-INTO InformationSession VALUES (4, 'zoom', TO_DATE('11/11/2022', 'MM/DD/YYYY'), 'Citadel')
-INTO InformationSession VALUES (5, 'zoom', TO_DATE('12/12/2022', 'MM/DD/YYYY'), 'Rippling')
-INTO OnlineAssessment VALUES 
-(1, 'Intern', 70, 'Easy', 4, 800, TO_DATE('5/2/2022', 'MM/DD/YYYY'), TO_DATE('5/9/2022', 'MM/DD/YYYY'), 1, 1)
-INTO OnlineAssessment VALUES 
-(2, 'Intern', 80, 'Medium', 4, 800, TO_DATE('3/4/2022', 'MM/DD/YYYY'), TO_DATE('5/1/2022', 'MM/DD/YYYY'), 2, 2)
-INTO OnlineAssessment VALUES 
-(3, 'Intern', 90, 'Hard', 4, 800, TO_DATE('5/6/2022', 'MM/DD/YYYY'), TO_DATE('5/19/2022', 'MM/DD/YYYY'), 3, 3)
-INTO OnlineAssessment VALUES 
-(4, 'Intern', 70, 'Medium', 4, 800, TO_DATE('5/7/2022', 'MM/DD/YYYY'), TO_DATE('5/9/2022', 'MM/DD/YYYY'), 3, 2)
-INTO OnlineAssessment VALUES 
-(5, 'Intern', 60, 'Easy', 4, 800, TO_DATE('5/8/2022', 'MM/DD/YYYY'), TO_DATE('6/9/2022', 'MM/DD/YYYY'), 3, 1)
-INTO PhoneScreen VALUES 
-(6, TO_DATE('5/1/2022', 'MM/DD/YYYY'), TO_DATE('10/30/2022', 'MM/DD/YYYY'), 1, 1)
-INTO PhoneScreen VALUES 
-(7, TO_DATE('6/2/2022', 'MM/DD/YYYY'), TO_DATE('11/20/2022', 'MM/DD/YYYY'), 2, 2)
-INTO PhoneScreen VALUES 
-(8, TO_DATE('7/2/2022', 'MM/DD/YYYY'), TO_DATE('8/10/2022', 'MM/DD/YYYY'), 3, 3)
-INTO PhoneScreen VALUES 
-(9, TO_DATE('8/2/2022', 'MM/DD/YYYY'), TO_DATE('8/10/2022', 'MM/DD/YYYY'), 3, 1)
-INTO PhoneScreen VALUES 
-(10, TO_DATE('6/2/2022', 'MM/DD/YYYY'), TO_DATE('6/1/2022', 'MM/DD/YYYY'), 3, 2)
-INTO OnsiteInterview VALUES 
-(11, TO_DATE('5/1/2022', 'MM/DD/YYYY'), TO_DATE('10/30/2022', 'MM/DD/YYYY'), 1, 1)
-INTO OnsiteInterview VALUES 
-(12, TO_DATE('6/2/2022', 'MM/DD/YYYY'), TO_DATE('11/20/2022', 'MM/DD/YYYY'), 2, 2)
-INTO OnsiteInterview VALUES 
-(13, TO_DATE('7/2/2022', 'MM/DD/YYYY'), TO_DATE('8/10/2022', 'MM/DD/YYYY'), 3, 3)
-INTO OnsiteInterview VALUES 
-(14, TO_DATE('8/2/2022', 'MM/DD/YYYY'), TO_DATE('8/10/2022', 'MM/DD/YYYY'), 3, 1)
-INTO OnsiteInterview VALUES 
-(15, TO_DATE('6/2/2022', 'MM/DD/YYYY'), TO_DATE('6/1/2022', 'MM/DD/YYYY'), 3, 2)
-INTO TeamMatching VALUES 
-(16, TO_DATE('5/1/2022', 'MM/DD/YYYY'), TO_DATE('10/30/2022', 'MM/DD/YYYY'), 1, 1)
-INTO TeamMatching VALUES 
-(17, TO_DATE('6/2/2022', 'MM/DD/YYYY'), TO_DATE('11/20/2022', 'MM/DD/YYYY'), 2, 2)
-INTO TeamMatching VALUES 
-(18, TO_DATE('7/2/2022', 'MM/DD/YYYY'), TO_DATE('8/10/2022', 'MM/DD/YYYY'), 3, 3)
-INTO TeamMatching VALUES 
-(19, TO_DATE('8/2/2022', 'MM/DD/YYYY'), TO_DATE('8/10/2022', 'MM/DD/YYYY'), 3, 1)
-INTO TeamMatching VALUES 
-(20, TO_DATE('6/2/2022', 'MM/DD/YYYY'), TO_DATE('6/1/2022', 'MM/DD/YYYY'), 3, 2)
-INTO Participate VALUES (1, 1, 1)
-INTO Participate VALUES (2, 2, 2)
-INTO Participate VALUES (3, 3, 3)
-INTO Participate VALUES (3, 1, 1)
-INTO Participate VALUES (3, 2, 2)
-INTO Host VALUES (1, 1) 
-INTO Host VALUES (2, 2)
-INTO Host VALUES (3, 3)
-INTO Host VALUES (4, 4)
-INTO Host VALUES (5, 5)
-SELECT 1 FROM DUAL;
+-- -- Populate tables
+-- INSERT ALL
+-- INTO Applicant VALUES (1, 'Steven', 'Li')
+-- INTO Applicant VALUES (2, 'Ethan', 'Lin') 
+-- INTO Applicant VALUES (3, 'Anton', 'Chen') 
+-- INTO Applicant VALUES (4, 'Raymond', 'Ng')
+-- INTO Applicant VALUES (5, 'Kanye', 'West')
+-- INTO Company VALUES ('Amazon', 'Vine', 'Vancouver', 'BC', 'Canada', 'V2A3A6')
+-- INTO Company VALUES ('Google', 'Heather', 'Vancouver', 'BC', 'Canada', 'V6H7A6')
+-- INTO Company VALUES ('Asana', 'Victoria', 'Vancouver', 'BC', 'Canada', 'V2H7U6')
+-- INTO Company VALUES ('Citadel', 'Queen', 'Vancouver', 'BC', 'Canada', 'V1N4B6')
+-- INTO Company VALUES ('Rippling', 'Oak', 'Vancouver', 'BC', 'Canada', 'V7A5A6')
+-- INTO Posting VALUES (1, 'Internship', 10, TO_DATE('10/22/2022', 'MM/DD/YYYY'), 'janitor', 'company bathroom', 'Asana')
+-- INTO Posting VALUES (2, 'Internship', 50, TO_DATE('5/1/2022', 'MM/DD/YYYY'), 'secretary', 'office', 'Google')
+-- INTO Posting VALUES (3, 'FullTime', 100, TO_DATE('12/4/2022', 'MM/DD/YYYY'), 'sales', 'office', 'Amazon')
+-- INTO Posting VALUES (4, 'FullTime', 500, TO_DATE('10/30/2022', 'MM/DD/YYYY'), 'worker', 'warehouse', 'Amazon')
+-- INTO Posting VALUES (5, 'Internship', 30, TO_DATE('2/28/2023', 'MM/DD/YYYY'), 'sorting documents', 'office', 'Google')
+-- INTO Recruiter VALUES (1, 'Kevin', 'Durant', 'Asana')
+-- INTO Recruiter VALUES (2, 'Kyrie', 'Irving', 'Google')
+-- INTO Recruiter VALUES (3, 'Jeff', 'Bezos', 'Amazon')
+-- INTO Recruiter VALUES (4, 'Lil', 'Pump', 'Rippling')
+-- INTO Recruiter VALUES (5, 'Kanye', 'West', 'Google')
+-- INTO Interviewer VALUES (1, 'James', 'Harden', 'Senior Engineer', 'Asana')
+-- INTO Interviewer VALUES (2, 'LeBron', 'James', 'Junior Engineer', 'Google')
+-- INTO Interviewer VALUES (3, 'Michael', 'Jordan', 'Junior Engineer', 'Amazon')
+-- INTO Interviewer VALUES (4, 'Stephen', 'Curry', 'Project Manager', 'Rippling')
+-- INTO Interviewer VALUES (5, 'Chris', 'Paus', 'Principal Engineer', 'Google')
+-- INTO JobApplication VALUES (1, 'stevenli.com/coverletter.pdf', 'stevenli.com/resume.pdf', 'Offer', 1, 1, 1, 'Asana')
+-- INTO JobApplication VALUES (2, 'ethanlin.com/coverletter.pdf', 'ethanlin.com/resume.pdf', 'Accepted', 2, 2, 2, 'Google')
+-- INTO JobApplication VALUES (3, 'antonchen.com/coverletter.pdf', 'antonchen.com/resume.pdf', 'Rejected', 3, 3, 3, 'Amazon')
+-- INTO JobApplication VALUES (4, 'antonchen.com/coverletter.pdf', 'antonchen.com/resume.pdf', 'Rejected', 3, 2, 2, 'Google')
+-- INTO JobApplication VALUES (5, 'antonchen.com/coverletter.pdf', 'antonchen.com/resume.pdf', 'Rejected', 3, 1, 1, 'Asana')
+-- INTO InformationSession VALUES (1, 'zoom', TO_DATE('5/1/2022', 'MM/DD/YYYY'), 'Asana')
+-- INTO InformationSession VALUES (2, 'zoom', TO_DATE('5/10/2022', 'MM/DD/YYYY'), 'Google')
+-- INTO InformationSession VALUES (3, 'zoom', TO_DATE('10/10/2022', 'MM/DD/YYYY'), 'Amazon')
+-- INTO InformationSession VALUES (4, 'zoom', TO_DATE('11/11/2022', 'MM/DD/YYYY'), 'Citadel')
+-- INTO InformationSession VALUES (5, 'zoom', TO_DATE('12/12/2022', 'MM/DD/YYYY'), 'Rippling')
+-- INTO OnlineAssessment VALUES 
+-- (1, 'Intern', 70, 'Easy', 4, 800, TO_DATE('5/2/2022', 'MM/DD/YYYY'), TO_DATE('5/9/2022', 'MM/DD/YYYY'), 1, 1)
+-- INTO OnlineAssessment VALUES 
+-- (2, 'Intern', 80, 'Medium', 4, 800, TO_DATE('3/4/2022', 'MM/DD/YYYY'), TO_DATE('5/1/2022', 'MM/DD/YYYY'), 2, 2)
+-- INTO OnlineAssessment VALUES 
+-- (3, 'Intern', 90, 'Hard', 4, 800, TO_DATE('5/6/2022', 'MM/DD/YYYY'), TO_DATE('5/19/2022', 'MM/DD/YYYY'), 3, 3)
+-- INTO OnlineAssessment VALUES 
+-- (4, 'Intern', 70, 'Medium', 4, 800, TO_DATE('5/7/2022', 'MM/DD/YYYY'), TO_DATE('5/9/2022', 'MM/DD/YYYY'), 3, 2)
+-- INTO OnlineAssessment VALUES 
+-- (5, 'Intern', 60, 'Easy', 4, 800, TO_DATE('5/8/2022', 'MM/DD/YYYY'), TO_DATE('6/9/2022', 'MM/DD/YYYY'), 3, 1)
+-- INTO PhoneScreen VALUES 
+-- (6, TO_DATE('5/1/2022', 'MM/DD/YYYY'), TO_DATE('10/30/2022', 'MM/DD/YYYY'), 1, 1)
+-- INTO PhoneScreen VALUES 
+-- (7, TO_DATE('6/2/2022', 'MM/DD/YYYY'), TO_DATE('11/20/2022', 'MM/DD/YYYY'), 2, 2)
+-- INTO PhoneScreen VALUES 
+-- (8, TO_DATE('7/2/2022', 'MM/DD/YYYY'), TO_DATE('8/10/2022', 'MM/DD/YYYY'), 3, 3)
+-- INTO PhoneScreen VALUES 
+-- (9, TO_DATE('8/2/2022', 'MM/DD/YYYY'), TO_DATE('8/10/2022', 'MM/DD/YYYY'), 3, 1)
+-- INTO PhoneScreen VALUES 
+-- (10, TO_DATE('6/2/2022', 'MM/DD/YYYY'), TO_DATE('6/1/2022', 'MM/DD/YYYY'), 3, 2)
+-- INTO OnsiteInterview VALUES 
+-- (11, TO_DATE('5/1/2022', 'MM/DD/YYYY'), TO_DATE('10/30/2022', 'MM/DD/YYYY'), 1, 1)
+-- INTO OnsiteInterview VALUES 
+-- (12, TO_DATE('6/2/2022', 'MM/DD/YYYY'), TO_DATE('11/20/2022', 'MM/DD/YYYY'), 2, 2)
+-- INTO OnsiteInterview VALUES 
+-- (13, TO_DATE('7/2/2022', 'MM/DD/YYYY'), TO_DATE('8/10/2022', 'MM/DD/YYYY'), 3, 3)
+-- INTO OnsiteInterview VALUES 
+-- (14, TO_DATE('8/2/2022', 'MM/DD/YYYY'), TO_DATE('8/10/2022', 'MM/DD/YYYY'), 3, 1)
+-- INTO OnsiteInterview VALUES 
+-- (15, TO_DATE('6/2/2022', 'MM/DD/YYYY'), TO_DATE('6/1/2022', 'MM/DD/YYYY'), 3, 2)
+-- INTO TeamMatching VALUES 
+-- (16, TO_DATE('5/1/2022', 'MM/DD/YYYY'), TO_DATE('10/30/2022', 'MM/DD/YYYY'), 1, 1)
+-- INTO TeamMatching VALUES 
+-- (17, TO_DATE('6/2/2022', 'MM/DD/YYYY'), TO_DATE('11/20/2022', 'MM/DD/YYYY'), 2, 2)
+-- INTO TeamMatching VALUES 
+-- (18, TO_DATE('7/2/2022', 'MM/DD/YYYY'), TO_DATE('8/10/2022', 'MM/DD/YYYY'), 3, 3)
+-- INTO TeamMatching VALUES 
+-- (19, TO_DATE('8/2/2022', 'MM/DD/YYYY'), TO_DATE('8/10/2022', 'MM/DD/YYYY'), 3, 1)
+-- INTO TeamMatching VALUES 
+-- (20, TO_DATE('6/2/2022', 'MM/DD/YYYY'), TO_DATE('6/1/2022', 'MM/DD/YYYY'), 3, 2)
+-- INTO Participate VALUES (1, 1, 1)
+-- INTO Participate VALUES (2, 2, 2)
+-- INTO Participate VALUES (3, 3, 3)
+-- INTO Participate VALUES (3, 1, 1)
+-- INTO Participate VALUES (3, 2, 2)
+-- INTO Host VALUES (1, 1) 
+-- INTO Host VALUES (2, 2)
+-- INTO Host VALUES (3, 3)
+-- INTO Host VALUES (4, 4)
+-- INTO Host VALUES (5, 5)
+-- SELECT 1 FROM DUAL;
