670234df0374fa3bbc18ccb9712cfa29eebe5549
UBC Student <azchen@thetis.students.cs.ubc.ca>
2022-03-30 04:02:53 -0700

    fix queries

--- milestone3/code/sql/queries.sql
+++ milestone3/code/sql/queries.sql
@@ -19,12 +19,15 @@ WHERE C.CompanyName = :company_name;
 
 -- UPDATE
 -- "Reject an applicant from all applied positions at a company."
-UPDATE Applications AP, Applicant A
-SET AP.Decision = 'rejected'
-WHERE A.ApplicantID = AP.ApplicantID 
-  AND A.FirstName = :first_name
-  AND A.LastName = :last_name
-  AND AP.CompanyName = :company_name;
+UPDATE JobApplication J
+SET J.Decision = 'Rejected'
+WHERE J.CompanyName = :company_name
+  AND J.ApplicantID IN (
+    SELECT A.ApplicantID 
+    FROM Applicant A
+    WHERE A.FirstName = :first_name
+      AND A.LastName = :last_name
+);
 
 -- SELECTION
 -- "Find all the information sessions at specified location."
@@ -34,46 +37,52 @@ WHERE I.Location IS :location;
 
 -- PROJECTION QUERY
 -- "Find the names of all companies which have internships available."
-SELECT I.CompanyName
+SELECT DISTINCT I.CompanyName
 FROM Internship I;
 
 -- JOIN QUERY
--- "Select all applicants who have been offered an internship position in a given location."
+-- "Select all applicants who will join a position in a given location."
 SELECT A.FirstName, A.LastName
-FROM Applicant A, Application AP, Internship I
-WHERE A.ApplicantID = AP.ApplicationID 
-  AND AP.PostingID = I.PostingID
-  AND I.Location LIKE :location;
+FROM Applicant A, JobApplication J, Posting P
+WHERE A.ApplicantID = J.ApplicantID 
+  AND J.PostingID = P.PostingID
+  AND P.PostingLocation = :location
+  AND J.Decision = 'Accepted';
 
 -- AGGREGATION
 -- "Find the number of recruiters for each company."
-SELECT R.CompanyName, COUNT(*)
+SELECT R.CompanyName, COUNT(*) AS NumRecruiters
 FROM Recruiter R
-GROUP BY R.CompanyName;
+GROUP BY R.CompanyName
+ORDER BY NumRecruiters DESC;
 
 -- NESTED AGGREGATION
--- "Find the company who has accepted the most interns."
-WITH InternCount AS (
-  SELECT CompanyName, COUNT(*) AS NumInterns
-  FROM Application AP, Internship I
-  WHERE AP.PostingId = I.PostingID 
-    AND (AP.Decision = 'Offer' OR AP.Decison = 'Accepted')
-  GROUP BY AP.CompanyName
+-- "Find the company who has given out the most offers."
+WITH OfferCount AS (
+  SELECT J.CompanyName, COUNT(*) AS NumOffers
+  FROM JobApplication J, Posting P
+  WHERE J.PostingID = P.PostingID 
+    AND (J.Decision = 'Offer' OR J.Decision = 'Accepted')
+  GROUP BY J.CompanyName
 )
-SELECT MAX(IC.NumInterns)
-FROM InternCount IC;
+SELECT OC.CompanyName, OC.NumOffers
+FROM OfferCount OC
+WHERE OC.NumOffers = (
+  SELECT MAX(OC2.NumOffers)
+  FROM OfferCount OC2
+);
 
 -- DIVISION
--- "Find all the applicants who have applied to every company that offers internships."
+-- "Find all the applicants who have applied to every company that has postings."
 SELECT A.FirstName, A.LastName
 FROM Applicant A
 WHERE NOT EXISTS (
   (
-    SELECT C.CompanyName 
-    FROM Company C;
-  ) EXCEPT (
-    SELECT AP.CompanyName
-    FROM Application AP
-    WHERE A.ApplicantID = AP.ApplicantID
+    SELECT P.CompanyName 
+    FROM Posting P
+  ) MINUS (
+    SELECT J.CompanyName
+    FROM JobApplication J
+    WHERE A.ApplicantID = J.ApplicantID
   )
 );
